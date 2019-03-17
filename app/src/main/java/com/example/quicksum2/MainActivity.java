f02003f3ee4bf7237e1ee955131085c8fd7ea2e6
jc319816 <jacob.negri@my.jcu.edu.au>
2019-03-17 21:17:10 +1000

    .

--- app/src/main/java/com/example/quicksum2/MainActivity.java
+++ app/src/main/java/com/example/quicksum2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.quicksum2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClicked(View view) {
+        Button button = (Button) view;
+
+        int number = Integer.parseInt(button.getText().toString());
+        sum += number;
+
+        TextView TEXTvIEW = (TextView) findViewById(R.id.sum);
+        String result = "" + sum;
+        textView.setText(result);
     }
 }
